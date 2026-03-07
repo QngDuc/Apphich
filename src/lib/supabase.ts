325e1a0040d05f475b4dfeddc5a17407137da65a
QngDuc <phamducquang717@gmail.com>
2026-03-07 16:23:14 +0700

    add note to every code

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,9 @@
+//NHẬP KIỂU NGƯỜI DÙNG TỪ THƯ VIỆN SUPABASE
 import {createClient} from '@supabase/supabase-js';
 
+// LẤY URL VÀ ANON KEY TỪ BIẾN MÔI TRƯỜNG ĐƯỢC ĐỊNH NGHĨA TRONG .ENV.LOCAL
 const supabaseURL = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
+// TẠO VÀ XUẤT ĐỐI TƯỢNG SUPABASE CLIENT SỬ DỤNG URL VÀ ANON KEY
 export const supabase = createClient(supabaseURL, supabaseAnonKey);
